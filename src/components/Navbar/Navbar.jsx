7290d5f307ac0f2a10f53be1283ce554322fe891
Matt Tierney <tierneybbq@gmail.com>
2023-02-25 17:28:41 -0600

    smallscreen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,6 +23,22 @@ const Navbar = () => (
        <div />
        <a href="/" className="p__opensans">Book Event</a>
        </div>
+
+       <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => {}}/>
+        
+        
+        <ul className="app__navbar-smallscreen-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#News">News</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+       </ul>
+       </div>
+       </div>
   </nav>
 );
 
